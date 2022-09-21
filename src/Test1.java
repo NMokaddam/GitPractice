086faada4dc0a30dd82fd95e1b94e206b13b5385
gauravm <gauravphuse1@gmail.com>
2022-09-21 17:12:31 +0530

    First commit.....

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=8;
+		int b=16;
+		
+	    a=a+b;
+	    b=a-b;
+	    a=a-b;
+	    System.out.println(a);
+	    System.out.println(b);
+	}
+
+}
