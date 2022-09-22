a8715703f02ddeb9996a4f98742fb159f390c2af
Navneet <mokddamnavneet92@gmail.com>
2022-09-22 16:05:14 +0530

    Addede additional checks...

--- src/Test2.java
+++ src/Test2.java
@@ -4,15 +4,15 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a=7;
-		int b=9;
+		int c=7;
+		int d=9;
 		int temp=0;
 		
-		temp=a;
-		a=b;
-		b=temp;
-		System.out.println(a);
-		System.out.println(b);
+		temp=c;
+		c=d;
+		d=temp;
+		System.out.println(c);
+		System.out.println(d);
 		
 		
 	}
