85017aabf2d617f617d5632c95f49ad269490733
Navneet <mokddamnavneet92@gmail.com>
2022-09-22 14:49:36 +0530

    commiting Test2 to Branch QaLocal

---
+++ src/Test2.java
@@ -0,0 +1,20 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=7;
+		int b=9;
+		int temp=0;
+		
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+	}
+
+}
